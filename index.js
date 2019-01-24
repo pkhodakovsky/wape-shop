fdf716c6ff1ab76294c3e69e8c11da2a9af7e77a
Pavel Khodakovsky <p.khodakovsky@viseven.com>
2019-01-24 17:42:35 +0200

    update(*)

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+import touch from 'touches';
 import IScroll from './src/js/iscroll';
 import TmRipple from './src/js/tmripple';
+
+const POPUP_VISIBLE_CLASS = 'visible';
 
 function initScrolls() {
 	const liquidsEl = document.getElementById('liquids');
@@ -7,6 +10,7 @@ function initScrolls() {
 		scrollX: true,
 		scrollY: false,
 		mouseWheel: true,
+		preventDefault: false,
 	});
 	window.liquidsScroll = liquidsScroll;
 }
@@ -17,5 +21,63 @@ function initTmripples() {
 	});
 }
 
+function initPopups() {
+	function openPopupHandler(event) {
+		const popupId = this.dataset.openPopupName;
+		popupWrapper.classList.add(POPUP_VISIBLE_CLASS);
+		popupWrapper.querySelector(`[data-popup-name="${popupId}"]`).classList.add(POPUP_VISIBLE_CLASS);
+	}
+
+	function closePopupHandler(event) {
+		const popupId = this.dataset.closePopupName;
+		popupWrapper.classList.remove(POPUP_VISIBLE_CLASS);
+		popupWrapper.querySelector(`[data-popup-name="${popupId}"]`).classList.remove(POPUP_VISIBLE_CLASS);
+	}
+
+	const popupWrapper = document.getElementById('popups');
+	const popupOpenButtons = document.querySelectorAll('[data-open-popup-name');
+	const popupCloseButtons = document.querySelectorAll('[data-close-popup-name');
+	popupOpenButtons.forEach(buttonEl => {
+		handleTap(buttonEl, openPopupHandler);
+	});
+	popupCloseButtons.forEach(buttonEl => {
+		handleTap(buttonEl, closePopupHandler);
+	});
+}
+
+function handleTap(element, handler) {
+	let isMoved = false;
+	let isTouched = false;
+
+	function startHandler(event) {
+		event.preventDefault();
+		isMoved = false;
+		isTouched = true;
+	}
+
+	function moveHandler(event) {
+		if (!isTouched) {
+			return;
+		}
+		isMoved = true;
+	}
+
+	function endHandler(event) {
+		isTouched = false;
+		if (isMoved) {
+			isMoved = false;
+			return;
+		}
+		isMoved = false;
+		handler.call(element, event);
+	}
+
+	touch(element)
+		.on('start', startHandler)
+		.on('move', moveHandler)
+		.on('end', endHandler);
+}
+
 initScrolls();
 initTmripples();
+initPopups();
