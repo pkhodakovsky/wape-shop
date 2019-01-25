739ee100e197bd20fc9f4e249f2c0e8e58de6fe8
Pavel Khodakovsky <p.khodakovsky@viseven.com>
2019-01-25 13:30:10 +0200

    update(*)

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-import touch from 'touches';
 import Hammer from 'hammerjs';
 import IScroll from './src/js/iscroll';
 import TmRipple from './src/js/tmripple';
@@ -38,58 +37,13 @@ function initPopups() {
 	const popupOpenButtons = document.querySelectorAll('[data-open-popup-name]');
 	const popupCloseButtons = document.querySelectorAll('[data-close-popup-name]');
 	popupOpenButtons.forEach(buttonEl => {
-		// handleTap(buttonEl, openPopupHandler);
 		const mc = new Hammer(buttonEl);
 		mc.on('tap', openPopupHandler.bind(buttonEl));
 	});
 	popupCloseButtons.forEach(buttonEl => {
-		// handleTap(buttonEl, closePopupHandler);
 		const mc = new Hammer(buttonEl);
 		mc.on('tap', closePopupHandler.bind(buttonEl));
 	});
-}
-
-function handleTap(element, handler) {
-	let isMoved = false;
-	let isTouched = false;
-	let possibleDiff = 20;
-	let prevPos = {
-		x: 0,
-		y: 0,
-	};
-
-	function startHandler(event, pos) {
-		event.preventDefault();
-		isMoved = false;
-		isTouched = true;
-		prevPos = {
-			x: pos[0],
-			y: pos[1],
-		};
-	}
-
-	function moveHandler(event, pos) {
-		if (!isTouched) {
-			return;
-		}
-		isMoved = true;
-	}
-
-	function endHandler(event, pos) {
-		isTouched = false;
-		if (isMoved) {
-			// if (Math.abs(pos[0] - prevPos.x) > possibleDiff || Math.abs(pos[1] - prevPos.y) > possibleDiff) {
-			isMoved = false;
-			return;
-		}
-		isMoved = false;
-		handler.call(element, event);
-	}
-
-	touch(element)
-		.on('start', startHandler)
-		.on('move', moveHandler)
-		.on('end', endHandler);
 }
 
 initScrolls();
