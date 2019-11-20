7bb629e7b616d8a61a1f810aae013de38eb5cf21
Pavel Khodakovsky <p.khodakovsky@viseven.com>
2019-11-20 10:12:38 +0200

    update(*):add components for ShopItem and LiquidItem

---
+++ src/components/LiquidItem.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="box">
+    <div class="image fit">
+      <img :src="image"
+           alt="" width="100%" height="auto"/>
+    </div>
+    <div class="content">
+      <header class="align-center">
+        <h2 v-html="title"></h2>
+      </header>
+      <hr/>
+      <ul>
+        <li v-for="(item, index) in list" :key="index" v-html="item"></li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LiquidItem',
+  props: {
+    image: String,
+    title: String,
+    list: Array,
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/ShopItem.vue
@@ -0,0 +1,13 @@
+<template>
+  
+</template>
+
+<script>
+  export default {
+    name: 'ShopItem',
+  };
+</script>
+
+<style scoped>
+
+</style>
